b613b58d32829b87f9995329414d1760a7e73fe5
MinhThang <Thanmn9403@gmail.com>
2024-02-26 22:21:48 +0700

    move function to utils file

--- src/components/LeftSide/MiniCalendar.tsx
+++ src/components/LeftSide/MiniCalendar.tsx
@@ -1,35 +1,10 @@
 import { space } from "postcss/lib/list";
 import React, { useEffect, useState } from "react";
 import { useDatePickerStore } from "../../stores/DatePickerStore";
+import { getDayofMonth } from "../../utils/utils";
 
 const MiniCalendar = () => {
   const { currentMonth } = useDatePickerStore();
-  let lastDayOfmonth = NaN;
-  let dateEndOfMonth: number[] = [];
-
-  const getDayofMonth = (day: Date) => {
-    const year = day.getFullYear();
-    const month = day.getMonth();
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    lastDayOfmonth = daysInMonth;
-    const firstDayOfMonth = new Date(
-      year,
-      month,
-      Math.floor(Math.random() * 3 + 1)
-    ).getDate();
-    let currentCountMonth = 0 - firstDayOfMonth;
-    return new Array(6).fill([]).map(() => {
-      return new Array(7).fill(null).map(() => {
-        const day = new Date(year, month, currentCountMonth).toUTCString();
-        const color =
-          currentCountMonth >= 1 && currentCountMonth <= daysInMonth
-            ? "text-black font-normal"
-            : "text-slate-400";
-        currentCountMonth++;
-        return { day, color };
-      });
-    });
-  };
 
   const dayOfWeek = ["sun", "mon", "tue", "wed", "thu", "pri", "sat"];
 
@@ -48,11 +23,7 @@ const MiniCalendar = () => {
               className=" w-full gap-2 grid grid-cols-7 text-center"
             >
               {week.map((data: any, index) => {
-                const today = new Date().toUTCString();
-                today === data.day && console.log("ok");
-                console.log(new Date(data.day).toUTCString().slice(0, 16));
-                console.log(today.slice(0, 16));
-
+                const today = new Date().toLocaleString();
                 return (
                   <div
                     key={index}
@@ -60,8 +31,8 @@ const MiniCalendar = () => {
                       console.log(new Date(data.day));
                     }}
                     className={`${data.color} text-black  cursor-pointer ${
-                      today.slice(0, 16) ===
-                        new Date(data.day).toUTCString().slice(0, 16) &&
+                      today.slice(0, 9) ===
+                        new Date(data.day).toLocaleString().slice(0, 9) &&
                       "rounded-full flex items-center justify-center w-7 h-7 bg-blue-500 text-white"
                     }`}
                   >
