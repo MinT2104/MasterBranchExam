9c39089e84d37131e7a721549bcee8cef8fb5030
MinhThang <Thanmn9403@gmail.com>
2024-02-26 22:54:52 +0700

    Add filter variable

--- src/stores/DatePickerStore.tsx
+++ src/stores/DatePickerStore.tsx
@@ -3,12 +3,32 @@ import { create } from "zustand";
 type datePickerStore = {
   currentMonth: Date;
   indexLastOfMonth: number;
+  currentFilter: number;
+  currentDayChosen: Date;
+  setCurrentDayChosen: (data: Date) => void;
+  currentWeekChosen: { day: string; color: string }[];
+  setCurrentWeekChosen: (data: { day: string; color: string }[]) => void;
+  setCurrentFilter: (data: number) => void;
   setIndexLastOfMonth: (data: number) => void;
   setNewMonth: (newMonth: Date) => void;
 };
 export const useDatePickerStore = create<datePickerStore>()((set, get) => ({
   currentMonth: new Date(),
   indexLastOfMonth: 0,
+  currentFilter: 2,
+  currentDayChosen: new Date(),
+  setCurrentDayChosen: (data) => {
+    set({
+      currentDayChosen: data,
+    });
+  },
+  currentWeekChosen: [],
+  setCurrentWeekChosen: (data) => {
+    set({ currentWeekChosen: data });
+  },
+  setCurrentFilter: (data) => {
+    set({ currentFilter: data });
+  },
   setIndexLastOfMonth: (data) => {
     set({ indexLastOfMonth: data });
   },
